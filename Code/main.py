25966840fde7ccbfb638032166c761f75458d967
Akhilrajan-V <Akhilrajan-V>
2022-11-11 08:45:28 -0500

    main draft

--- Code/main.py
+++ Code/main.py
@@ -1,6 +1,7 @@
-import scipy.io as sc
-import cv2 as cv
-mat = sc.loadmat('./Data/data.mat')
-data = mat['face']
-cv.imshow('face', data[:,:,2])
-cv.waitKey(0)
+import numpy as np
+
+"""
+
+# call functions and methods
+"""
+
